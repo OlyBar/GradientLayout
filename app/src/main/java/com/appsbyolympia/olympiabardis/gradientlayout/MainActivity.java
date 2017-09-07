43a5feb554fe1cfc706bd9322d6a0e767406cca3
Olympia Bardis <olybar10@yahoo.com>
2017-09-06 21:07:39 -0400

    Update MainActivity.java

--- app/src/main/java/com/appsbyolympia/olympiabardis/gradientlayout/MainActivity.java
+++ app/src/main/java/com/appsbyolympia/olympiabardis/gradientlayout/MainActivity.java
@@ -9,5 +9,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+                final Button button = (Button) findViewById(R.id.button_shadow);
+        button.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View view, MotionEvent event) {
+                if(event.getAction() == MotionEvent.ACTION_UP) {
+                    button.setBackgroundColor(Color.RED);
+                } else if(event.getAction() == MotionEvent.ACTION_DOWN) {
+                    button.setBackgroundColor(Color.BLUE);
+                }
+                return false;
+            }
+
+        });
     }
 }
